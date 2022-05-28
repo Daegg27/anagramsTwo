a6856270a21facb0fd7a2e5186fc0817c5ca49e8
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-27 22:59:37 -0700

    javascript

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,27 @@
 exports.anagramsFor = function(word, listOfWords) {
 
+    let sortedStrings = [];
+    let anagramArray = [];
+    let finalAnswer;
+
+    for (let i = 0; i < listOfWords.length; i++){
+        sortedStrings.push(listOfWords[i].toLowerCase().split("").sort().join(""));
+    }
+    finalAnswer = word.toLowerCase().split("").sort().join("");
+    
+    // console.log(sortedStrings);
+    // console.log(finalAnswer);
+    for (let k = 0; k < sortedStrings.length; k++){
+        if (finalAnswer === sortedStrings[k]){
+            anagramArray.push(listOfWords[k]);
+        }
+    }
+   
+    return anagramArray;
+    
+
+
+
+
 };
+// exports.anagramsFor("hellojf", ["helEFEa", "DFfsAW", "DHAkedD", "AbdaAW"]);
